ac82cdd21dd9bf48fc7d44a7397098129ac8d95f
bajricharun <harunbajric@protonmail.com>
2020-02-09 23:51:10 +0100

    new

---
+++ application/views/pages/home.php
@@ -0,0 +1,153 @@
+<div class='selectors'>
+	<div class='selector'>
+		<div class='new'> 
+			<div class='num'>I</div>
+			<div class='plus' onclick = 'show(0);'><a>+</a></div>
+		</div>
+		<div class='text'>
+			<h1>I</h1>
+			<p>
+					Trazeci duboko u sebi mir<br> 
+					Dosao sam do zakljucka <br>
+					Da ova bozanska stvorenja<br> 
+					Koja zovemo ljudi <br>
+					Stvaraju veliki strah u nama.<br> 
+<br>
+					Da nas dovode do ludila, <br>
+					Tjeraju nas u najmracnije dijelove naseg uma <br>
+					I tada ispoljavamo ono najgore. <br><br>
+					
+					Pokusavao sam pronaci stalozenost<br>
+					Naci mir, naci smisao svega <br>
+					I nasao sam ga na mjestu zaboravljenom	<br>
+					Od strane svake osobe. <br>
+<br>
+					Cak i pastiri koji su nekada bili tu	
+<br>			Vise ne znaju za ovo mjesto, niti zele cuti za njega.<br>
+			</p>
+		</div>
+	</div>
+	<div class='selector'> 
+		<div class='new'>
+			<div class='num'>II</div>
+			<div class='plus' onclick='show(1);'><a>+</a></div>
+		</div>
+		<div class='text'>
+			<h1>II</h1>
+			<p>
+				Nikada nisam znao da cu naci mir<br> 
+				U ovoj zabiti, zaboravljenoj i od Boga.<br>
+				Mir i tisina su se konacno stopili <br>
+				U jednu cjelinu i ispunjavaju me.<br>
+<br>
+				Uistinu je covjek bice kojem je 
+<br>		Potrebna njega i ljubav<br>
+				Dolje medju svim tim ljudima<br>
+				To nikada necu dobiti.<br>
+<br>
+				Uvijek cu biti usamljen i cudan
+<br>		Uvijek cu biti jebeni cudak <br>
+				Koji je nekad davno napisao<br>
+				Jednu dobru knjigu<br>
+				Sve ostalo sto je uradio je proslo neprimjeceno.<br>
+<br>
+				Postao sam onaj cudak koji ima sumanute ideje.<br>
+			</p>
+		</div>
+	</div>	
+	<div class='selector'> 
+		<div class='new'>
+			<div class='num'>III</div>
+			<div class='plus' onclick='show(2);'><a>+</a></div>
+		</div>
+		<div class='text'>
+			<h1>III</h1>
+			<p>
+					Razmisljajuci o ljudskoj prirodi<br>
+					Otisao samo duboko u sebe<br>
+					Toliko duboko da sam zaboravio	<br>
+					Na vrijeme.<br>
+<br>
+					Prolazili su sati	<br>
+					Prolazili su dani <br>
+					Sve dok me jednoga dana nije <br>
+					Iz dubokih misli probudio hitac<br>
+<br>
+					Hitac ocaja, hitac koji je oznacavao <br>
+					Kraj mog mira, <br>
+					Kraj mog spokoja, <br>
+					Kraj moje slobode!<br>
+				<br>
+					Nikada nisam bio spreman <br>
+					Na ovaj tren, nisam znao da ce do njega doci.<br>
+			</p>
+		</div>
+	</div>
+	<div class='selector'>
+		<div class='new'> 
+			<div class='num'>IV</div>
+			<div class='plus' onclick='show(3);'><a>+</a></div>
+		</div>
+		<div class='text'>
+			<h1>IV</h1>
+			<p>
+					Dok je rat harao ovom zemljom <br>
+					Ja sam duboko u sebi razmisljao <br>
+					O prirodi svega<br>
+					To jeste, dok rat nije dosao i na moja vrata. <br>
+					<br>
+					Izasao sam da izvidim sta se desilo<br>
+					I nasao sam jadnog jelena koji lezi <br>
+					Na podu, boreci se iz sve snage <br>
+					Da jos jednom pogleda <br>
+					Svoj dom, svoj spokoj sve ono sto mu je nekada davalo mir sto mu je davalo slobodu<br>
+					<br>
+					U tom jelenu sam vidio sve nas<br>
+					Svaku osobu koja je ikada umrla<br>
+					Kako posljednji put gleda u voljene<br>
+					U svoj dom i u svoju slobodu<br>
+					I zali za svim onim sto je bilo i sto je moglo da bude.<br>
+			</p>
+		</div>
+	</div>  
+	<div class='selector'> 
+		<div class='new'>
+			<div class='num'>V</div>
+			<div class='plus' onclick='show(4);'><a>+</a></div>
+		</div>
+		<div class='text'>
+			<h1>V</h1>
+			<p>
+				Iz jelena je lila krv,  <br>
+				Topla krv koja je do jucer <br>
+				Grijala njegovo tijelo, vrila u njemu. <br>
+				Topa krv koja je do jucer tekla <br>
+				Kroz vene bica koje nikada <br>
+				Nikome nista nije skrivilo <br>
+				Kroz vene bica koje je zeljelo <br>
+				Jos jedan tren da se divi bozijoj kreaciji <br>
+				Koja je njemu davala mir i slobodu. <br>
+				<br>
+				Lezao je ukoceno <br>
+				Dok je njegov lovac gledao svoj plijen <br>
+				I divio se svom ulovu<br>
+				Savrseni jelen, <br>
+				Njegovi rogovi ce pristajati uz smedji zid njegove kolibe. <br>
+			</p>
+		</div>
+	</div>
+</div>
+
+<script>
+	
+function show(i) {
+	var x = document.getElementsByClassName('plus');
+	var y = document.getElementsByClassName('text');
+	if (y[i].style.display === 'none') {
+		y[i].style.display = 'block';
+	}
+	else {
+		y[i].style.display = 'none';
+	}
+}
+</script>
---
+++ application/views/templates/header.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel='stylesheet' href='http://localhost:5500/assets/css/style.css'>
+		<title>Nova zora</title>
+	</head>
+	<body>
+			<nav>
+			<h1>Nova zora - Harun Bajric</h1>
+			</nav>
